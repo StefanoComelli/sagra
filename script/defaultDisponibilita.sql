212ffe8633d90e2c1ec2104091bfa15dc2d7f843
StefanoComelli <stefanocomelli@gmail.com>
2015-05-23 10:47:23 +0200

    20150523_1046 Inserito il Listino Ufficiale in Base Dati

--- script/defaultDisponibilita.sql
+++ script/defaultDisponibilita.sql
@@ -1,8 +1,12 @@
-insert into prodottigiornaliera (idGiorno, idProdotto)
-select idGiorno, idProdotto
-from listinoprodotti, giorni
+DELETE FROM prodottigiornaliera;
 
-update prodottigiornaliera
-set disponibilita =999, quantitaVenduta=0, quantitaWarning=0,
+INSERT INTO prodottigiornaliera (idGiorno, idProdotto)
+SELECT idGiorno, idProdotto
+FROM listinoprodotti, giorni;
+
+UPDATE prodottigiornaliera
+SET disponibilita =999, quantitaVenduta=0, quantitaWarning=0,
 scontoGiorno=0, sospensione=0
-where idGiorno>0
+WHERE idGiorno>0;
+
+COMMIT;
