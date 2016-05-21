961cb6c4db5d1a3d275cbbfeb854fc6469de3282
Stefano <Stefano@192.168.1.129>
2016-05-21 23:06:50 +0200

    Stato ordine: in tabella Commesse, idStatoOrdine: 1 = In corso, 2 = Stampato Tolta gestione listino anomalio giornata geometri

--- script/defaultDisponibilita.sql
+++ script/defaultDisponibilita.sql
@@ -9,13 +9,13 @@ SET disponibilita =999, quantitaVenduta=0, quantitaWarning=0,
 scontoGiorno=0, sospensione=0
 WHERE idGiorno>0;
 
-delete from prodottigiornaliera
-where idGiorno=1 
-and idProdotto not in(1010,2000,2001,2002,2006,2012,
-3002,3003,3004,3001,202,204,502,703,706);
+--delete from prodottigiornaliera
+--where idGiorno=1 
+--and idProdotto not in(1010,2000,2001,2002,2006,2012,
+--3002,3003,3004,3001,202,204,502,703,706);
 
-delete from prodottigiornaliera
-where idGiorno>1 
-and idProdotto in(1010);
+--delete from prodottigiornaliera
+--where idGiorno>1 
+--and idProdotto in(1010);
 
 COMMIT;
